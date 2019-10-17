fc035556007378b39332af06c3323e4281261133
PhuongDuy <kudophuongduy@gmail.com>
2019-10-17 22:04:46 +0700

    Storing past moves

--- src/index.js
+++ src/index.js
@@ -68,29 +68,49 @@ class Game extends React.Component {
 			history: [{
 				squares: Array(9).fill(null)
 			}],
-		isNextX: true
+			stepNumber: 0,
+			isNextX: true
 		};
 	};
 	handleClick(i) {
-		const history = this.state.history;
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
 		if (calculateWinner(squares) || squares[i])
-		return;
+			return;
 		squares[i] = this.state.isNextX ? 'X' : 'O';
 	
 		this.setState({
 			history: history.concat([{
 				squares: squares,
 			}]),
+			stepNumber: history.length,
 			isNextX: !this.state.isNextX
 		});
+	  };
+
+	jumpTo(step) {
+		  this.setState({
+			  stepNumber: step,
+			  isNextX: (step % 2) === 0
+		  });
 	  }
+
     render() {
 		const history = this.state.history;
-		const current = history[history.length-1];
+		const current = history[this.state.stepNumber];
 		const winner = calculateWinner(current.squares);
 		let status;
+		const moves = history.map((step, move) => {
+			const desc = move ? 
+			'Go to move #' +move : 
+			'Game start';
+			return (
+				<li key = {move}>
+					<button onClick = {() => this.jumpTo(move)}>{desc}</button>
+				</li>
+			) 
+		});
 		if (winner) 
 			status = 'Winner: ' + winner;
 		else 
@@ -104,7 +124,7 @@ class Game extends React.Component {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
